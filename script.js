4e5912b3877601382e267b9871741031574a940f
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-11-04 23:05:07 +0800

    Add a dynamic output for calculation process

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const input = document.getElementById("input");
 const button = document.getElementById("button");
 const answer = document.getElementById("answer");
+const text = document.getElementById("text");
 
 //Constants
 const MULTIPLIER = 3;
@@ -34,8 +35,10 @@ const validateBarcode = (barcode) => {
   }
 
   if (errorMessages.length > 0) {
-    throw new Error(`Please enter a valid barcode: ${errorMessages.join(", ")}!`);
-  }  
+    throw new Error(
+      `Please enter a valid barcode: ${errorMessages.join(", ")}!`
+    );
+  }
 };
 
 const calculateCheckDigit = (barcode) => {
@@ -47,10 +50,10 @@ const calculateCheckDigit = (barcode) => {
   I noticed that if the length of the even and odd are the same, the one that
   is multiplied to 3 is always the evenSum, else it would be the oddSum. 
   */
-  const baseSum = evenLength === oddLength ? evenSum : oddSum; 
+  const baseSum = evenLength === oddLength ? evenSum : oddSum;
 
   //If the sum used in baseSum is evenSum, then oddSum should be used to the other one, vice versa.
-  const otherSum = baseSum === evenSum ? oddSum : evenSum; 
+  const otherSum = baseSum === evenSum ? oddSum : evenSum;
 
   //Get the last digit from the baseSum then multiply it by 3.
   let multiplyResult = MULTIPLIER * (baseSum % MODULO_CONSTANT);
@@ -60,9 +63,141 @@ const calculateCheckDigit = (barcode) => {
     (multiplyResult % MODULO_CONSTANT) + (otherSum % MODULO_CONSTANT);
 
   //Return zero if sumResult is equal 0 and 10, else get the last digit of the sumResult then minus that to 10.
-  return sumResult === 0 || sumResult === 10
-    ? 0
-    : MODULO_CONSTANT - (sumResult % MODULO_CONSTANT);
+
+  const checkDigit =
+    sumResult === 0 || sumResult === 10
+      ? 0
+      : MODULO_CONSTANT - (sumResult % MODULO_CONSTANT);
+
+  return {
+    checkDigit,
+    evenSum,
+    oddSum,
+    baseSum,
+    otherSum,
+    multiplyResult,
+    sumResult,
+  };
+};
+
+const outputCalculation = (barcode) => {
+  const isEAN = barcode.length === 12;
+  console.log(isEAN);
+
+  const span = printSpan(barcode, isEAN);
+
+  return `<p>
+
+  ${isEAN ? "Odd digits (EAN Barcode)" : "Even digits (UPC Barcode)"}
+  <br/>
+
+  Separate:
+  ${span.sequenceSum}  
+  <br /><br />
+
+  Operate: <br />
+  ${isEAN ? span.sequenceSumOdd : span.sequenceSumEven}
+  <br />
+  ${isEAN ? span.sequenceSumEven : span.sequenceSumOdd}
+  <br /><br />
+
+  Calculate: <br />
+  ${span.multiplySequence}
+  <br />
+  ${span.sumSequence}
+  <br />
+
+  ${span.moduloSequence}
+<br />
+
+${span.checkDigit}
+  </p>`;
+};
+
+const printSpan = (barcode, whatBarcode) => {
+  const result = calculateCheckDigit(barcode);
+  const barcodeChar = barcode.split("");
+
+  let sumOdd = result.oddSum.toString();
+  sumOdd =
+    sumOdd.length > 1
+      ? `${sumOdd[0]}<span class="lastOdd">${sumOdd[1]}</span>`
+      : `<span class="lastOdd">${sumOdd}</span>`;
+
+  let sumEven = result.evenSum.toString();
+  sumEven =
+    sumEven.length > 1
+      ? `${sumEven[0]}<span class="lastEven">${sumEven[1]}</span>`
+      : `<span class="lastEven">${sumEven}</span>`;
+
+  let multiplyResult = result.multiplyResult.toString();
+  multiplyResult =
+    multiplyResult.length > 1
+      ? `${multiplyResult[0]}<span class="lastMultiply">${multiplyResult[1]}</span>`
+      : `<span class="lastMultiply">${multiplyResult}</span>`;
+
+  let sumResult = result.sumResult.toString();
+  sumResult =
+    sumResult.length > 1
+      ? `${sumResult[0]}<span class="lastAdd">${sumResult[1]}</span>`
+      : `<span class="lastAdd">${sumResult}</span>`;
+
+  let sequenceSum = barcodeChar
+    .map((digit, index) => {
+      return `<span class="${index % 2 === 0 ? "odd" : "even"} ${
+        index === barcodeChar.length - 1 ? "modulo" : ""
+      }">${digit}</span>`;
+    })
+    .join("");
+
+  let sequenceSumOdd =
+    barcodeChar
+      .map((digit, index) =>
+        index % 2 === 0
+          ? `<span class="odd">${digit}</span>${
+              index === barcodeChar.length - 2 ? " = " : " + "
+            }`
+          : ""
+      )
+      .join("") + sumOdd;
+
+  let sequenceSumEven =
+    barcodeChar
+      .map((digit, index) =>
+        index % 2 !== 0
+          ? `<span class="even">${digit}</span>${
+              index === barcodeChar.length - 1 ? " = " : " + "
+            }`
+          : ""
+      )
+      .join("") + sumEven;
+
+  let lastMultiplyResult = result.multiplyResult.toString().slice(-1);
+  let lastSumEven = result.evenSum.toString().slice(-1);
+  let lastSumOdd = result.oddSum.toString().slice(-1);
+  let lastSumResult = result.sumResult.toString().slice(-1);
+
+  let multiplySequence = `<span class="constant">3</span> x <span class="lastEven">${
+    whatBarcode ? lastSumEven : lastSumOdd
+  }</span> = ${multiplyResult}`;
+
+  let sumSequence = `<span class="lastMultiply">${lastMultiplyResult}</span> + <span class="lastOdd">${
+    whatBarcode ? lastSumOdd : lastSumEven
+  }</span> = ${sumResult}`;
+
+  let moduloSequence = `<span class="modulo"></span> + <span class="lastAdd">${lastSumResult}</span> = <span class="constant">10</span>`;
+
+  let checkDigit = `<span class="modulo"></span> <span class="checkDigit">= ${result.checkDigit}</span>`;
+
+  return {
+    sequenceSum,
+    sequenceSumEven,
+    sequenceSumOdd,
+    multiplySequence,
+    sumSequence,
+    moduloSequence,
+    checkDigit,
+  };
 };
 
 button.addEventListener("click", () => {
@@ -71,9 +206,14 @@ button.addEventListener("click", () => {
   try {
     validateBarcode(barcode);
 
-    let checkDigit = calculateCheckDigit(barcode);
+    let result = calculateCheckDigit(barcode);
 
-    answer.textContent = `Barcode Check Digit: ${checkDigit}`;
+    answer.textContent = `Barcode Check Digit: ${result.checkDigit}`;
+
+    text.innerHTML =
+      `<p class="description">
+        This is the process to arrive at the check digit:
+      </p>` + outputCalculation(barcode);
   } catch (error) {
     answer.textContent = error.message;
   }
